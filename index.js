d7c85e03d3999c339951474f3aac38fbf480801f
shahana <rafiyashahana@gmail.com>
2023-12-18 14:44:06 +0300

    added media queries

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+// const slidesContainer = document.getElementById("slides-container");
+// const slide = document.querySelector(".slide");
+// // const prevButton = document.getElementById("slide-arrow-prev");
+// const nextButton = document.getElementById("slide-arrow-next");
 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
+// nextButton.addEventListener("click", () => {
+//   const slideWidth = slide.clientWidth;
+//   slidesContainer.scrollLeft += slideWidth;
+// });
 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
+// prevButton.addEventListener("click", () => {
+//   const slideWidth = slide.clientWidth;
+//   slidesContainer.scrollLeft -= slideWidth;
+// });
+
+let slideIndex = 1;
+function showSlide(index) {
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName("dot");
+
+  if (index < 1) {
+    slideIndex = slides.length;
+  } else if (index > slides.length) {
+    slideIndex = 1;
+  } else {
+    slideIndex = index;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+
+  dots[slideIndex - 1].classList.add("active");
+}
+
+showSlide(slideIndex);
+
+function nextSlide() {
+  showSlide(slideIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(slideIndex - 1);
+}
+
+function currentSlide(index) {
+  showSlide(index);
+}
